b7bef8194ae0b9ed187c093a5ba41e29bf4f4576
Sean <devlinsf@devlinsolutions.com>
2009-06-01 08:42:33 -0400

    Added Clipboard Utilities

---
+++ src/lib/devlinsf/clip_utils.clj
@@ -0,0 +1,48 @@
+(ns lib.devlinsf.clip-utils)
+  ;(:import (java.awt.datatransfer Clipboard 
+;				  ClipboardOwner
+;				  Transferable
+;				  StringSelection
+;				  DataFlavor
+;				  UnsupportedFlavorException)
+;	   java.awt.Toolkit))
+
+;java.awt.datatransfer.
+
+(defn get-sys-clip
+  []
+  (. (java.awt.Toolkit/getDefaultToolkit) getSystemClipboard))
+
+(defn get-clip
+  []
+  (let [clipboard (get-sys-clip)]
+    (if clipboard
+      (let [contents (. clipboard getContents nil)]
+	(cond 
+	 (nil? contents) nil
+	 (not (. contents isDataFlavorSupported java.awt.datatransfer.DataFlavor/stringFlavor)) nil
+	 true (. contents getTransferData java.awt.datatransfer.DataFlavor/stringFlavor))))))
+
+(defn set-clip!
+  [input-string]
+  (if input-string
+    (let [clipboard (get-sys-clip)]
+      (if clipboard
+	(do
+	  (let [selection (java.awt.datatransfer.StringSelection. input-string)]
+	    (. clipboard setContents selection nil))
+	  input-string)))))
+
+(defmacro defclip
+  [symbol-name]
+  `(def ~symbol-name (get-clip)))
+
+(defn read-clip
+  []
+  (read-string (get-clip)))
+
+(defn eval-clip
+  []
+  (eval (read-clip)))
+
+(+ 2 2)
