957844a83df025cb0fdb54d076054896cd960bee
Sean <devlinsf@devlinsolutions.com>
2009-06-10 03:09:04 -0400

    Updated model-utils/README.mardown.  Fixed definition bug in model-utils.clj

--- docs/model-utils/README.markdown
+++ docs/model-utils/README.markdown
@@ -18,22 +18,68 @@ This macro creates the following methods.
 #filters
 
 * def-filter
+
+A thin wrapper over defn
+
 * def-filter-factory
+
+When a filter needs to be curried...
 
 #mappings
 
 * def-mapping
+
+A thin wrapper over defn
+
 * def-mapping-factory
+
+When a mapping needs to be curried...
 
 #transforms
 
 Transforms take a hash-map in and return a map.  Think of them as decorators for an entire object, not just one or two methods.
 
+##`(def-transform [name doc-string? & params])`
+
+Takes a name, an optional doc-string, and a flat list of key/value pairs.  Defines a function _name_-transform.
+
+Sample Usage:
+
+	user=> (def-transform counter "Counts the arguments in a map" :count count)
+	#'user/counter-transform
+	user=> (counter-transform {:a "A" :b "B"})
+	{:count 2, :a "A", :b "B"}
+	user=> (counter-transform {:a "A" :b "B" :count 0})
+	{:a "A", :b "B", :count 3}
+	
+	user=> (def-transform counter :count count)
+	#'user/counter-transform
+	user=> (def-transform title :title #(titleize (% :title)) :name #(titleize(% :name)))
+	#'user/title-transform
+	user=> (def sample-map {:name "sean devlin" :title "hacking clojure"})
+	#'user/sample-map
+	user=> (title-transform sample-map)
+	{:name "Sean Devlin", :title "Hacking Clojure"}
+	user=> ((comp title-transform counter-transform) sample-map)
+	{:count 2, :name "Sean Devlin", :title "Hacking Clojure"}
+	user=> ((apply comp [title-transform counter-transform]) sample-map)
+	{:count 2, :name "Sean Devlin", :title "Hacking Clojure"}
+
 #projections
 
-Projections take a hash-map and turn in into a vector.
+Projections take a hash-map and turn in into a vector.  Combined with transforms they make a 1-2 punch of awesomeness.
+
+##`(def-projection [name doc-string? & params])`
+
+Takes a name, an optional doc-string, and a list of keywords.  Defines a function _name_-projection that expects a map as an input.
+Each key is retrieved in order, and collected into a vector.  The vector is the returned.
+
+Also, a variable _name_-columns is created, and the list of params is stored there.
 
 #standard-functions
 
 * keys-subset-mf
 * vals-subset-mf
+
+* multi-projection
+Used to concatenate mapping operations.
--- src/lib/devlinsf/model_utils.clj
+++ src/lib/devlinsf/model_utils.clj
@@ -5,10 +5,25 @@
 	clojure.contrib.sql
 	clojure.contrib.with-ns))
 
+;(defn multi-projection
+;  [& projections]
+;  (fn [input-map] (vec (reduce concat (map #(% input-map) projections)))))
+
 (defn if-doc
   [doc-string?]
   (if doc-string?
     (str "\n\n  " doc-string?)))
+
+(defn find-params
+  [& input-list]
+  (if (= (class (first input-list)) java.lang.String)
+		     (rest input-list)
+		     input-list))
+
+(defn find-doc-string
+  [& input-list]
+  (if (= (class (first input-list)) java.lang.String)
+    (first input-list)))
 
 (defmacro def-filter
   ([name params body] (def-filter name nil params body))
@@ -67,53 +82,7 @@
   "This function is used to retrieve standard values based on input keys.  The resulting mapping should return a vector."
   [& input-keys]
   (fn[input-map] (vec (map #(input-map %) input-keys))))
-
-(defn find-params
-  [& input-list]
-  (if (= (class (first input-list)) java.lang.String)
-		     (rest input-list)
-		     input-list))
-
-(defn find-doc-string
-  [& input-list]
-  (if (= (class (first input-list)) java.lang.String)
-    (first input-list)))
   
-
-(defmacro def-projection
-  [name & input-keys]
-  (let [header-name (symbol (str name "-columns"))
-	column-keys (find-params input-keys)
-	input-symbol (gensym "input_")
-	doc-string? (str (if-doc (find-doc-string input-keys))
-			  "This projection applies to the following keys:\n  * " 
-			  (str-join "\n  * " (map #(str-rest(str %)) column-keys))) ]
-    `(do
-       (def ~header-name [~@column-keys])
-       (def-mapping ~name ~doc-string? [~input-symbol] ((vals-subset-mf ~@column-keys) ~input-symbol)))))
-
-(defn multi-projection
-  [& projections]
-  (fn [input-map] (vec (reduce concat (map #(% input-map) projections)))))
-
-(defmacro def-transform
-  [name & input-keys]
-  (let [transform-symbol (symbol (str name "-transform"))
-	param-map (apply hash-map (apply find-params input-keys))
-	doc-string? (find-doc-string input-keys)
-	input-map-symbol (gensym "input-map_")
-	fn-map-symbol (gensym "fn-map_")]
-    `(defn ~transform-symbol [~input-map-symbol]
-       ~doc-string?
-       (apply assoc ~input-map-symbol
-	     (reduce concat 
-		     (map 
-		      (fn [~fn-map-symbol]
-			(list 
-			 (first ~fn-map-symbol) 
-			 ((second ~fn-map-symbol) ~input-map-symbol)))
-		      ~param-map))))))
-
 (defmacro create-finder-fn
   [db-con table-name]
   (let [base-name (keywordize table-name)
@@ -184,3 +153,33 @@
 		     (create-update-fn ~qualified-db ~table-name)
 		     (create-delete-fn ~qualified-db ~table-name)))))))
 
+
+(defmacro def-projection
+  [name & input-keys]
+  (let [header-name (symbol (str name "-columns"))
+	column-keys (apply find-params input-keys)
+	input-symbol (gensym "input_")
+	doc-string? (str (if-doc (apply find-doc-string input-keys))
+			  "This projection applies to the following keys:\n  * " 
+			  (str-join "\n  * " (map #(str-rest(str %)) column-keys))) ]
+    `(do
+       (def ~header-name [~@column-keys])
+       (def-mapping ~name ~doc-string? [~input-symbol] ((vals-subset-mf ~@column-keys) ~input-symbol)))))
+
+
+ (defmacro def-transform
+  [name & input-keys]
+  (let [transform-symbol (symbol (str name "-transform"))
+	param-map (apply hash-map (apply find-params input-keys))
+	doc-string? (str (apply find-doc-string input-keys))
+	input-map-symbol (gensym "input-map_")
+	fn-map-symbol (gensym "fn-map_")]
+    `(defn ~transform-symbol ~doc-string? [~input-map-symbol]
+       (apply assoc ~input-map-symbol
+	     (reduce concat 
+		     (map 
+		      (fn [~fn-map-symbol]
+			(list 
+			 (first ~fn-map-symbol) 
+			 ((second ~fn-map-symbol) ~input-map-symbol)))
+		      ~param-map))))))
