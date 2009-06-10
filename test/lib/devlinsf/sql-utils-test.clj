7c35399a6b3504bb03228b7fb2f566a5985ef264
Sean <devlinsf@devlinsolutions.com>
2009-06-10 01:41:06 -0400

    Added test/.../sql-utils-test.clj

---
+++ test/lib/devlinsf/sql-utils-test.clj
@@ -0,0 +1,18 @@
+(ns lib.devlinsf.test-contrib.sql-utils
+    (:use clojure.contrib.test-is
+          lib.devlinsf.sql-utils))
+
+(deftest test-keyword-id
+  (is (= (where-clause {:id 1}) "id=1")))
+
+(deftest test-string-id
+  (is (= (where-clause {"id" 1}) "id=1")))
+
+(deftest test-keyword-in
+  (is (= (where-clause {:id [1 2]}) "id IN (1, 2)")))
+
+(deftest test-sql-and
+  (is (= (where-clause {:id 1 :name "Sean"}) "id=1 AND name=\"Sean\"")))
+
+(deftest test-array-in
+  (is (= (where-clause {:name ["Sean" "Bill"]}) "name IN (\"Sean\", \"Bill\")")))
