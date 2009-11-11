cd59e7fb0c81ecdcafe0d89637b75c7182ce745f
Sean <devlinsf@devlinsolutions.com>
2009-11-10 23:57:39 -0500

    Added messages namespace

---
+++ src/lib/sfd/swing/messages.clj
@@ -0,0 +1,56 @@
+(ns lib.sfd.swing.messages
+  (:import [javax.swing JOptionPane]))
+
+(defn messenger-factory
+  [message-type]
+  (fn [m t] (JOptionPane/showMessageDialog nil m t message-type nil)))
+
+(defn plain-message
+  "Creates a dialog that shows a plain message.  Will block the thread it is in.  Returns nil."
+  ([message] (show-message message nil))
+  ([message title] ((messenger-factory JOptionPane/PLAIN_MESSAGE) message title)))
+
+(defn info-message
+  "Creates a dialog that shows an informational message.  Will block the thread it is in.  Returns nil."
+  ([message] (show-message message nil))
+  ([message title] ((messenger-factory JOptionPane/INFORMATION_MESSAGE) message title)))
+
+(defn warning-message
+  "Creates a dialog that shows a warning message.  Will block the thread it is in.  Returns nil."
+  ([message] (show-message message nil))
+  ([message title] ((messenger-factory JOptionPane/WARNING_MESSAGE) message title)))
+
+(defn error-message
+  "Creates a dialog that shows an error message.  Will block the thread it is in.  Returns nil."
+  ([message] (show-message message nil))
+  ([message title] ((messenger-factory JOptionPane/ERROR_MESSAGE) message title)))
+
+(defn question-message
+  "Creates a dialog that shows a question message.  Will block the thread it is in.  Returns nil."
+  ([message] (show-message message nil))
+  ([message title] ((messenger-factory JOptionPane/QUESTION_MESSAGE) message title)))
+
+;A map to turn integers into keywords
+(def yes-no-decoder {JOptionPane/YES_OPTION :yes
+		     JOptionPane/NO_OPTION :no})
+		     
+(defn yes-no-dialog
+  "Creates an yes/no dialog.  Will block the thread it is in.  Returns :yes or :no"
+  ([message] (yes-no-dialog message nil))
+  ([message title] (yes-no-decoder 
+	  (JOptionPane/showConfirmDialog nil message title JOptionPane/YES_NO_OPTION))))
+
+(defn yes-no-cancel-dialog
+  "Creates an yes/no/cancel dialog.  Will block the thread it is in.  Returns :yes or :no, or nil if cancelled"
+  ([message] (yes-no-cancel-dialog message nil))
+  ([message title] (yes-no-decoder
+	  (JOptionPane/showConfirmDialog nil message title JOptionPane/YES_NO_CANCEL_OPTION))))
+
+(defn input-dialog
+  "Creates an input dialog.  Will block the thread it is in.  Returns the value entered as a String, nil if cancelled."
+  [message] (JOptionPane/showInputDialog message))
+
+(defn dropdown-dialog
+  "Creates a dropdown dialog.  Will block the thread it is in.  Returns the value selected, or nil if cancelled."
+  ([message title coll] (dropdown-dialog message title coll nil))
+  ([message title coll default] (JOptionPane/showInputDialog nil message title JOptionPane/QUESTION_MESSAGE nil (into-array coll) default)))
