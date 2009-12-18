853d534f55465eadcc68a63f92b24939af314f03
Sean <devlinsf@devlinsolutions.com>
2009-12-18 00:56:22 -0500

    Added same, multi-same, alterante and split.  Seriously bad ass hacking.

--- src/lib/sfd/core.clj
+++ src/lib/sfd/core.clj
@@ -84,3 +84,114 @@ transient function f to a persistent collection.
 Returns a peristent collection." 
 	:arglists '([f! & args])} 
      quick! (visitor transient persistent!))
+
+(defn- same-dispatch [coll]
+  (cond
+   (map? coll) :default
+   (sorted? coll) :sorted-set
+   (set? coll) :hash-set
+   (vector? coll) :vector
+   (string? coll) :string
+   true :default))
+
+(defmulti same (fn [ & args] (same-dispatch (last args))))
+
+(defmethod same :default
+  [hof f & args]
+  (apply hof f args))
+
+(defmethod same :vector
+  [hof f & args]
+  (vec (apply hof f args)))
+
+(defmethod same :hash-set
+  [hof f & args]
+  (set (apply hof f args)))
+
+(defmethod same :string
+  [hof f & args]
+  (apply str (apply hof f args)))
+
+(defmethod same :sorted-set
+  [hof f & args]
+  (let [c (.comparator (first args))]
+    (apply sorted-set-by c (apply hof f args))))
+
+(defmulti multi-same (fn [ & args] (same-dispatch (last args))))
+
+(defmethod multi-same :default
+  [hof f & args]
+  (apply hof f args))
+
+(defmethod multi-same :string
+  [hof f & args]
+  (map (partial apply str) (apply hof f args)))
+
+(defmethod multi-same :vector
+  [hof f & args]
+  (map vec (apply hof f args)))
+
+(defmethod multi-same :hash-set
+  [hof f & args]
+  (map set (apply hof f args)))
+
+(defmethod multi-same :sorted-set
+  [hof f & args]
+  (let [c (.comparator (first args))]
+    (map (partial apply sorted-set-by c) (apply hof f args))))
+
+(defn alternate
+  "Splits a collection by matching a predicate.  The predicate match is greedy.  It's like a regex partition."
+  [pred coll]
+  (lazy-seq
+    (loop [output []
+	   r-coll coll]
+      (let [item (first r-coll)
+	    split-coll (split-with #(= (nil? (pred item)) (nil? (pred %))) r-coll)
+	    new-coll (conj output (first split-coll))
+	    non-matching-coll (second split-coll)]
+	(if (empty? non-matching-coll)
+	  new-coll
+	  (recur new-coll non-matching-coll))))))
+
+(defn split
+  "Splits like a regex split."
+  [pred coll]
+  (remove (comp pred first) (alternate pred coll)))
+
+(defn take-last
+  "Mirrors drop-last.  Works like tail, the classic *nix command."
+  [n coll]
+  (drop (- (count coll) n) coll))
+
+(defn take-until
+  "Returns a lazy sequence of successive items from coll while
+  (pred item) returns false. pred must be free of side-effects."
+  [pred coll]
+  (take-while (complement pred) coll))
+
+(defn drop-until
+  "Returns a lazy sequence of the items in coll starting from the first
+  item for which (pred item) returns true."
+  [pred coll]
+  (drop-while (complement pred) coll))
+
+(defn rotate
+  "Take a collection and left rotates it n steps.  If n is negative, the
+collection is rotated right. Executes in O(n) time."
+  [n coll]
+  (let [c (count coll)]
+    (take c (drop (- c (mod n c)) (cycle coll)))))
+
+(defn rotate-while
+  "Rotates a collection left while (pred item) is true.  Will return a unrotated
+sequence if (pred item) is never true. Executes in O(n) time."
+  [pred coll]
+  (let [head (drop-while pred coll)]
+    (take (count coll) (concat head coll))))
+
+(defn rotate-until
+  "Rotates a collection left while (pred item) is nil.  Will return a unrotated
+sequence if (pred item) is always true. Executes in O(n) time."
+  [pred coll]
+  (rotate-while (complement pred) coll))
